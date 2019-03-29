73e89cbf849345d5682ee839620ff0fb627ebf0f
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2019-03-29 11:00:17 +0100

    Add an install script for me

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # MachineSetup #
 
 Ansible configuration for my Fedora machines.
+
+## Installation
+
+### If you're me
+
+```shell
+$ curl https://raw.githubusercontent.com/mattiasb/MachineSetup/roles/go.sh \
+    2>/dev/null | sudo bash
+```
+
+### If you're not me
+
+**Never** install stuff from the internet by doing a `curl <url> | sudo bash`.
---
+++ go.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+URL=https://github.com/mattiasb/MachineSetup.git
+BRANCH=roles
+# REPO_DIR="$(dirname "${HOME}/Code/${URL#https://}")/$(basename "${URL%.git}")"
+REPO_DIR="/tmp/MachineSetup"
+
+# Install ansible (in the background)
+sudo dnf install -y ansible python
+
+# Clone the code
+if [ ! -d "${REPO_DIR}" ]; then
+    mkdir -p "$(dirname "${REPO_DIR}")"
+    git clone --branch "${BRANCH}" "${URL}" "${REPO_DIR}"
+fi
+cd "${REPO_DIR}"
+git branch --set-upstream-to="origin/${BRANCH}" "${BRANCH}"
+git config user.name "Mattias Bengtsson"
+git config user.email "mattias.jc.bengtsson@gmail.com"
+git pull --rebase
+
+# Set hostname
+# TODO: Move this to Ansible
+if [[ "$(systemd-detect-virt)" == kvm ]]; then
+    sudo hostnamectl set-hostname machinesetup-test
+    sudo dnf install -y openssh-server
+    sudo systemctl enable --now sshd
+    sudo systemctl restart avahi-daemon
+else
+    case "$(cat /sys/devices/virtual/dmi/id/product_uuid)" in
+        4c4c4544-0054-4e10-8043-c7c04f565a31)
+            sudo hostnamectl set-hostname mattiasb-dell-E6540
+            return
+            ;;
+        1afaa71f-393e-11e8-9379-8c164560bb35)
+            sudo hostnamectl set-hostname mattiasb-lenovo-yoga920
+            return
+            ;;
+
+        *)
+            echo Unknown hardware!
+            exit 3
+    esac
+fi
+
+ansible-playbook ./site.yml --limit "$(hostname)"
