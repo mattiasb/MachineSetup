6c18c733bc9daa07486fa8ba12d6e59ba503e809
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2026-02-03 01:16:04 +0100

    sysconf: Fix a ShellCheck warning

--- roles/sysconf/files/etc/profile.d/zzzz-profile.sh
+++ roles/sysconf/files/etc/profile.d/zzzz-profile.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 
 # Source settings file
-if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/bash/profile" ]; then
+if [ -f "${XDG_CONFIG_HOME:-${HOME}/.config}/bash/profile" ]; then
     # shellcheck disable=SC1091
-    . "${XDG_CONFIG_HOME:-$HOME/.config}/bash/profile"
+    . "${XDG_CONFIG_HOME:-${HOME}/.config}/bash/profile"
 fi
